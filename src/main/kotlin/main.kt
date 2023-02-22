efbe307eded7695e7805b2d39ae510f0497ea4e3
Konstantin Kokov <superstarforit@gmail.com>
2023-02-22 18:25:52 +0300

    add numbers that end in "11"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,8 @@ fun main() {
 
     val likes: Int = readln().toInt()
 
-    if (likes == 1 || likes.toString().endsWith("1") && likes != 11) println("Понравилось $likes человеку!")
+    if (likes.toString().length > 2 && likes.toString().endsWith("11")) println("Понравилось $likes человекам!")
     else if (likes.toString().endsWith("2") || likes.toString().endsWith("3") || likes.toString().endsWith("4")) println("Понравилось $likes людям!")
+    else if (likes == 1 || likes.toString().endsWith("1") && likes != 11) println("Понравилось $likes человеку!")
     else println("Понравилось $likes человекам!")
 }
